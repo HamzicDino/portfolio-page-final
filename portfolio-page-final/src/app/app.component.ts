19c467c3b47572645a8c0da9388d585b2c2d2cc6
hamzd2 <hamzd2@bfh.ch>
2023-11-27 12:55:06 +0100

    title change

--- portfolio-page-final/src/app/app.component.ts
+++ portfolio-page-final/src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'portfolio-page-final';
+  title = 'Portfolio-Page von D. Hamzic';
 }
