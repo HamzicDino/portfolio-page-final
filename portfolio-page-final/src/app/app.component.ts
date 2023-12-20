9e59556d65660792adbb1b659bcfd8632b990376
hamzd2 <hamzd2@bfh.ch>
2023-12-20 19:38:35 +0100

    Commits

--- portfolio-page-final/src/app/app.component.ts
+++ portfolio-page-final/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true
 })
 export class AppComponent {
   title = 'Portfolio-Page von D. Hamzic';
